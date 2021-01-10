c11ada61d79db364b270448e18730c2aef484aa8
Yash <yaswanth.iddibani@gmail.com>
2021-01-10 16:18:56 +0000

    NPM INIT

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-console.log("Starting");
+// console.log("Starting");
 
-setTimeout(() => {
-  console.log("2 second timer");
-}, 2000);
+// setTimeout(() => {
+//   console.log("2 second timer");
+// }, 2000);
 
-setTimeout(() => console.log("0 second timer"), 0);
+// setTimeout(() => console.log("0 second timer"), 0);
 
-console.log("Stopping");
+// console.log("Stopping");
